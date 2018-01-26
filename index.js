17e489fc0c1bab1f4256b9fcab76a6b982e97a37
Adam Jones <adam.jones@ucla.edu>
2018-01-25 17:28:44 -0800

    Image upload

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const PORT = process.env.PORT || 5000;
 
 let bodyParser = require('body-parser');
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 let app = express()
+  .use(allowCrossDomain)
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
