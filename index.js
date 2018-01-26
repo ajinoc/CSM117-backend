d71f5bd302c910d0aa3957486e26f90fe2d289c3
Adam Jones <adam.jones@ucla.edu>
2018-01-25 17:16:23 -0800

    Image upload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
+let bodyParser = require('body-parser');
+
+let app express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/post/image", function(req, res) {
+    var image = req.body.image;
+    console.log(image);
+});
