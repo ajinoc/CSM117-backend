c9aa629631fa1dc5f66ed2c4d3ef2acf08ca1b4b
Adam Jones <adam.jones@ucla.edu>
2018-01-25 17:45:27 -0800

    Image upload

--- index.js
+++ index.js
@@ -4,31 +4,20 @@ const PORT = process.env.PORT || 5000;
 
 let bodyParser = require('body-parser');
 
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+let app = express();
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-let app = express()
-  .use(allowCrossDomain)
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post("/post/image", function(req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", function(req, res) {
     let image = req.body.image;
-    console.log(req.body);
     console.log(image);
-   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
