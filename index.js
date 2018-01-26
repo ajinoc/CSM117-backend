1a7b2993c61fc3325c3d2c84386f36e3f2bf0e19
Adam Jones <adam.jones@ucla.edu>
2018-01-25 17:18:54 -0800

    Image upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 let bodyParser = require('body-parser');
 
-let app express()
+let app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
