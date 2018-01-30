c25f1a9e71f201ec8d26134ecb2a3ceb45a6b85a
Adam Jones <adam.jones@ucla.edu>
2018-01-30 00:54:15 -0800

    Commit

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ let bodyParser = require('body-parser');
 
 let app = express();
 
-/*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});*/
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
