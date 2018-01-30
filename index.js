59a349fa971b2b65881ec735f5130beedaade57f
Adam Jones <adam.jones@ucla.edu>
2018-01-30 01:13:41 -0800

    Commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "null");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
