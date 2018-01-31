9e34db9a1648c8b7c82498d5729a7eac7d1eb99b
Adam Jones <adam.jones@ucla.edu>
2018-01-31 14:56:29 -0800

    Commit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const path = require('path');
 const PORT = process.env.PORT || 5000;
-
-let bodyParser = require('body-parser');
 
 let app = express();
 
@@ -13,14 +10,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post("/", function(req, res) {
-    console.log(req);
-    res.send("Successful post");
 });
 
 let server = http.createServer(app);
